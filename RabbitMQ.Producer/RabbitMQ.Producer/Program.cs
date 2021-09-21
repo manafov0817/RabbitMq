011c6d6dba195ab50d7093c3634658d0f28fb9b8
manafov0817 <manafov0817@gmail.com>
2021-09-21 14:35:42 +0400

    first commit

---
+++ RabbitMQ.Producer/RabbitMQ.Producer/Program.cs
@@ -0,0 +1,39 @@
+﻿using Newtonsoft.Json;
+using RabbitMQ.Client;
+using System;
+using System.Text;
+
+namespace RabbitMQ.Producer
+{
+    static class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory()
+            {
+                Uri = new Uri("amqp://guest:guest@localhost:5672")
+            };
+
+            using var connection = factory.CreateConnection();
+
+            using var channel = connection.CreateModel();
+
+            channel.QueueDeclare(
+                "demo-queue",
+                durable: true,
+                exclusive: false,
+                autoDelete: false,
+                arguments: null
+                );
+
+            for (int i = 0; i < 5000; i++)
+            {
+                var message = new { Name = $"Producer {i+1}", Message = "Hello!" };
+
+                var body = Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(message));
+
+                channel.BasicPublish("", "demo-queue", null, body);
+            }
+        }
+    }
+}
